d9655098e4dd150f75871eb04f03367970332cc1
Jintops <jintops2003@gmail.com>
2025-07-11 18:33:02 +0530

    backend setup

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ const app=express();
 app.use("/",(req,res)=>{
     res.send("hoccoccco")
 })
+const PORT=7777
 
-
-app.listen(7777,()=>{
+app.listen(PORT,()=>{
     console.log("app successfully listening")
 })
 
