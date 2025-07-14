af9136c99562e54bc6cf2cb94b8e34fa7172aec2
Jintops <jintops2003@gmail.com>
2025-07-14 19:41:55 +0530

    security to cookies

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const User = require("../models/user");
 const userRouter = express.Router();
-const jwt=require('jsonwebtoken')
-const bcrypt = require('bcrypt')
-
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 userRouter.post("/signup", async (req, res) => {
   try {
-    const { firstName, emailId, password,number,gender,lastName,photoUrl } = req.body;
-    const passwordHash=await bcrypt.hash(password,10)
-    const user=await User.findOne({emailId:emailId})
+    const { firstName, emailId, password, number, gender, lastName, photoUrl } =
+      req.body;
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = await User.findOne({ emailId: emailId });
 
-    if(user){
-        return res.json({
+    if (user) {
+      return res.json({
         success: false,
         message: "User Already exists with the same email! Please try again",
       });
@@ -21,58 +21,68 @@ userRouter.post("/signup", async (req, res) => {
     const newUser = new User({
       firstName,
       emailId,
-      password:passwordHash,
+      password: passwordHash,
       number,
       gender,
       lastName,
-      photoUrl
+      photoUrl,
     });
     const data = await newUser.save();
 
-    const token=await jwt.sign({_id:data._id},"JINTO@123",{expiresIn:'1h'})
-    res.cookie("token",token,{ expires: new Date(Date.now() + 900000) })
+    const token = await jwt.sign({ _id: data._id }, "JINTO@123", {
+      expiresIn: "1h",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      expires: new Date(Date.now() + 60 * 60 * 1000),
+    });
 
-    res.json({ message: "user data successfully saved", data: data });
+    res.json({ success: true, message: "user data successfully saved", data });
   } catch (err) {
     res.status(400).send("ERROR :" + err.message);
   }
 });
 
+userRouter.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
 
-userRouter.post("/login",async(req,res)=>{
-  try{
-    const {emailId,password}=req.body
-
-    const user=await User.findOne({emailId:emailId});
-    if(!user){
-   return res.json({
-        success:false,
-        message:"User not  found"
-      })
-    }
-
-  const isValidPassword=await bcrypt.compare(password,user.password)
-
-    if(!isValidPassword){
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
       return res.json({
-        success:false,
-        message:"pasword is not correct"
-      })
-    }else{
-      const token=await jwt.sign({_id:user._id},"JINTO@123",{expiresIn:'1h'})
-      res.cookie("token",token,{ expires: new Date(Date.now() + 900000) })   
-      res.json({message:"login success",user})
+        success: false,
+        message: "User not  found",
+      });
     }
 
-  }catch(err){
-    res.status(400).send("ERROR"+err.message)
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return res.json({
+        success: false,
+        message: "pasword is not correct",
+      });
+    } else {
+      const { password, ...safeUser } = user._doc;
+      const token = await jwt.sign({ _id: user._id }, "JINTO@123", {
+        expiresIn: "1h",
+      });
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+        expires: new Date(Date.now() + 60 * 60 * 1000),
+      });
+      res.json({ success: true, message: "login success", user: safeUser });
+    }
+  } catch (err) {
+    res.status(400).send("ERROR" + err.message);
   }
-})
+});
 
-
-userRouter.post("/logout",async(req,res)=>{
-  res.cookie("token",null,{expires: new Date(Date.now())})
-  res.send("logout success")
-})
+userRouter.post("/logout", async (req, res) => {
+  res.clearCookie("token");
+  res.status(200).json({ message: "Logout successful" });
+});
 
 module.exports = userRouter;
