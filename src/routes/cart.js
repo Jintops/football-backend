55a5dfc2d6ab6edacdc70bef3f99728b8a905ce4
Jintops <jintops2003@gmail.com>
2025-07-20 18:48:34 +0530

    remove all items from cart

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -71,72 +71,81 @@ cartRouter.get("/cartItems", userAuth, async (req, res) => {
   }
 });
 
-// cartRouter.patch("/deleteCartItem/:productId", userAuth, async (req, res) => {
-//   try {
-//     const user = req.user;
-//     const { productId } = req.params;
-
-//     const updateItem = await Cart.findOneAndUpdate(
-//       { userId: user._id },
-//       { $pull: { items: { productId: productId } } },
-//       { new: true }
-//     ).populate("items.productId");
-
-//     if (!updateItem) {
-//       return res
-//         .status(404)
-//         .json({ success: false, message: "Cart not found" });
-//     }
-
-//     if (updateItem.items.length === 0) {
-//       await Cart.findByIdAndDelete(updateItem._id);
-//       return res
-//         .status(200)
-//         .json({
-//           success: true,
-//           message: "Item removed and cart is now empty. Cart deleted.",
-//         });
-//     }
-
-//     res.status(200).json({ success: true, data: updateItem });
-//   } catch (err) {
-//     res.status(400).send("ERROR :" + err.message);
-//   }
-// });
-
 cartRouter.patch("/deleteCartItem/:productId", userAuth, async (req, res) => {
   try {
     const user = req.user;
     const { productId } = req.params;
 
-    const cart = await Cart.findOne({ userId: user._id });
+    const updateItem = await Cart.findOneAndUpdate(
+      { userId: user._id },
+      { $pull: { items: { productId: productId } } },
+      { new: true }
+    ).populate("items.productId");
 
-    if (!cart) {
-      return res.status(404).json({ success: false, message: "Cart not found" });
+    if (!updateItem) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Cart not found" });
     }
 
-    // Filter out the item
-    cart.items = cart.items.filter(
-      (item) => item.productId.toString() !== productId
-    );
-
-    // Delete cart if no items left
-    if (cart.items.length === 0) {
-      await Cart.findByIdAndDelete(cart._id);
-      return res.status(200).json({
-        success: true,
-        message: "Item removed and cart is now empty. Cart deleted.",
-      });
+    if (updateItem.items.length === 0) {
+      await Cart.findByIdAndDelete(updateItem._id);
+      return res
+        .status(200)
+        .json({
+          success: true,
+          message: "Item removed and cart is now empty. Cart deleted.",
+        });
     }
 
-    const updatedCart = await cart.save();
-    const populatedCart = await Cart.findById(updatedCart._id).populate("items.productId");
-
-    res.status(200).json({ success: true, data: populatedCart });
+    res.status(200).json({ success: true, data: updateItem });
   } catch (err) {
-    res.status(400).send("ERROR: " + err.message);
+    res.status(400).send("ERROR :" + err.message);
   }
 });
 
+// cartRouter.patch("/deleteCartItem/:productId", userAuth, async (req, res) => {
+//   try {
+//     const user = req.user;
+//     const { productId } = req.params;
+//     const cart = await Cart.findOne({ userId: user._id });
+//     if (!cart) {
+//       return res.status(404).json({ success: false, message: "Cart not found" });
+//     }
+//     // Filter out the item
+//     cart.items = cart.items.filter(
+//       (item) => item.productId.toString() !== productId
+//     );
+//     // Delete cart if no items left
+//     if (cart.items.length === 0) {
+//       await Cart.findByIdAndDelete(cart._id);
+//       return res.status(200).json({
+//         success: true,
+//         message: "Item removed and cart is now empty. Cart deleted.",
+//       });
+//     }
+//     const updatedCart = await cart.save();
+//     const populatedCart = await Cart.findById(updatedCart._id).populate("items.productId");
+//     res.status(200).json({ success: true, data: populatedCart });
+//   } catch (err) {
+//     res.status(400).send("ERROR: " + err.message);
+//   }
+// });
+
+cartRouter.delete("/removeCart",userAuth,async(req,res)=>{
+  try{
+    
+    const user=req.user;
+    const cart=await Cart.findOneAndDelete({userId:user._id})
+   
+    if(!cart){
+      return res.status(404).json({success:false,message:"no items"})
+    }
+
+    res.status(200).json({success:true,message:"removed all items"})
+  }catch(err){
+    res.status(400).send("ERROR :"+err.message)
+  }
+})
 
 module.exports = cartRouter;
