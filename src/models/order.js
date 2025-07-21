9e2197d72d4aa089b76d911685a308fb012c39fe
Jintops <jintops2003@gmail.com>
2025-07-21 16:04:58 +0530

    updated orderModel

--- src/models/order.js
+++ src/models/order.js
@@ -2,20 +2,37 @@ const mongoose=require('mongoose');
 
 const orderSchema=new mongoose.Schema({
 
-userId:String,
-cartItem:{
-    title:String,
-    price:Number,
-    imageUrl:String,
-    quantity:Number,
+userId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User",
+    required:true,
+},
+cartItems:[{
+    title:{type:String,required:true},
+    price:{type:Number,required:true},
+    imageUrl:{type:String,required:true},
+    quantity:{type:Number,required:true},
+    
+}],
+address:{
+    name:{type:String,required:true},
+    address:{type:String,required:true},
+    pincode:{type:Number,required:true},
+    place:{type:String,required:true},
+    phone:{type:String,required:true},
+},
+orderStatus:{
+    type:String,
+    default:'pending'
+},
+paymentMethod:{
+    type:String,
+    enum:['Cash on Delivery','Online'],
     required:true
 },
-address:{
-    name:String,
-    address:String,
-    pincode:Number,
-    place:String,
-    phone:String
+totalAmount:{
+    type:String,
+    required:true
 }
 
 },{timestamps:true});
