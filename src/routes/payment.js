812b8472bf6e94e3916a6aca2727f80ba7e8167b
Jintops <jintops2003@gmail.com>
2025-08-15 17:59:38 +0530

    api for razorpay

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -1,9 +1,21 @@
 const express=require('express');
 const { userAuth } = require('../middlewares/auth');
 const paymentRouter=express.Router();
+const razorpayInstance=require('../utils/razorpay')
+
 
 paymentRouter.post("/payment/create",userAuth,async(req,res)=>{
     try{
+
+     const order=await razorpayInstance.orders.create({
+  "amount": 50000,
+  "currency": "INR",
+  "receipt": "receipt#1",
+  "notes": {
+    "firstName": "value3",
+    "lastName": "value2"
+  }
+})
 
     }catch(err){
         res.status(400).send("ERROR"+err.message)
