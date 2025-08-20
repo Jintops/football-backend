aef2e15e5a4568f8dcb6cbd4f96972268b572823
Jintops <jintops2003@gmail.com>
2025-08-20 15:07:07 +0530

    corner cases of review

--- src/routes/review.js
+++ src/routes/review.js
@@ -1,24 +1,32 @@
 const express=require('express');
 const { userAuth } = require('../middlewares/auth');
 const Review = require('../models/review');
+const Product = require('../models/product');
 
 const reviewRouter=express.Router();
 
 reviewRouter.post("/addReview",userAuth,async(req,res)=>{
     try{
         const user=req.user
-        const {productId,reviewMessage,rating,likesCount}=req.body
+        const {productId,reviewMessage,rating}=req.body
         
-        
+        const product=await Product.findById(productId);
+        if(!product){
+           return res.status(404).json({success:false,message:"Product Not Found"})
+        }
+          if (rating < 1 || rating > 5) {
+      return res.status(400).json({ success: false, message: "Rating must be between 1 and 5" });
+    }
          
         const review=new Review({
-            productId,
+            productId:product._id,
             userId:user._id,
             firstName:user.firstName,
             reviewMessage,
             rating,
             likesCount
         })
+
         
 const data=await review.save();
 res.status(200).json({success:true,data:data})
