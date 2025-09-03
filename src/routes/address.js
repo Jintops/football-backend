ea3724726ebeba9df9198207d07477743ea46db4
Jintops <jintops2003@gmail.com>
2025-09-03 14:47:07 +0530

    delete address

--- src/routes/address.js
+++ src/routes/address.js
@@ -73,5 +73,24 @@ addressRouter.put("/editAddress/:id",userAuth,async(req,res)=>{
     }
 })
 
+addressRouter.delete("/deleteAddress/:id",userAuth,async(req,res)=>{
+    try{
+        const user=req.user
+        const {id}=req.params
+        const address=await Address.findById(id)
+        if(!address){
+            return res.status(404).json({success:false,message:"No address found"})
+        }
+         if (address.userId.toString() !== user._id.toString()) {
+      return res.status(403).json({ success: false, message: "Not authorized to delete this address" });
+    }
+    await address.deleteOne();
+    
+        res.status(200).json({success:true,message:"Deleted successfully"})
+    }catch(err){
+        res.status(500).send("ERROR :"+err.message)
+    }
+})
+
 
 module.exports=addressRouter;
